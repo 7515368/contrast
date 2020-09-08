331266e0b5ac1ae823f96e014ed0a6c940d2da9a
emile <blitsau@gmail.com>
2020-09-08 16:57:49 +0300

    refactor, improvement, modification

--- source/src/js/Prices/parseData.js
+++ source/src/js/Prices/parseData.js
@@ -11,15 +11,25 @@ const parseData = () => {
             id,
             name: element.children[0].getAttribute("data-service-name"),
             price: element.children[1].getAttribute("data-service-price"),
+            unit: element.children[2].getAttribute("data-service-unit"),
             parentSection: sectionName,
         };
     });
 
-    const sections = sectionsNames.map((section, id) => ({id, name: section, isActive : true}));
+    const sections = sectionsNames.map((section, id) => ({ id, name: section, isActive: true }));
+
+    const icons = {};
+    const iconsContainer = document.querySelector("[data-icons]");
+
+    icons.checked = iconsContainer.querySelector("[data-icon-checked]");
+    icons.circle = iconsContainer.querySelector("[data-icon-circle]");
+    icons.cross = iconsContainer.querySelector("[data-icon-cross]");
+    icons.close = iconsContainer.querySelector("[data-icon-close]");
 
     return {
         sections,
         items,
+        icons
     };
 };
 export default parseData;
