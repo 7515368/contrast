442331c3866f96dfcb8032582d099b6fc0ffe636
pasha.khoroz <pashakotov999@gmail.com>
2020-09-09 02:16:30 +0300

    !

--- source/src/js/main.js
+++ source/src/js/main.js
@@ -20,8 +20,8 @@ function closeAllModals() {
 	setTimeout(function() {
 		$('.popup-block.active').removeClass('active', function() {
 		}).trigger('afterCloseModal');
-
 	}, 200);
+	
 }
 
 function closeModal(hrefModal) {
@@ -30,8 +30,10 @@ function closeModal(hrefModal) {
 	setTimeout(function() {
 		$(hrefModal).removeClass('active', function() {
 		}).trigger('afterCloseModal');
-
 	}, 200);
+
+	fullpage_api.setAllowScrolling(true);
+
 }
 
 $(document).keydown(function(event) { 
@@ -77,8 +79,49 @@ $(document.body).on('click','[data-toggle="modal-dismiss"]',function(e) {
 	closeModal($(this).parents('.popup-block')[0]);
 });
 
-$('.popup-block__slider').slick({
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	arrows: false
-});
+// $('.popup-block__slider').slick({
+// 	slidesToShow: 1,
+// 	slidesToScroll: 1,
+// 	arrows: false
+// });
+
+if ($('.popup-block__slider').length > 0) {
+	$('.popup-block__slider').each(function() {
+		var thisElem = $(this);
+
+		var thisElemSlidesCount = $(this).children().length;
+
+		var thisAllCountBlock = $(this).parents('.popup-block__slider-wrapper').find('.info-block__switch').find('.all-count');
+		var thisCurrentCountBlock = $(this).parents('.popup-block__slider-wrapper').find('.info-block__switch').find('.current');
+		
+		
+		// $(this).parents('.popup-block__slider-wrapper').find('.info-block__switch').find('.footer__portfolio-back').slick("slickPrev");
+		// $(this).parents('.popup-block__slider-wrapper').find('.info-block__switch').find('.footer__portfolio-forward').slick("slickNext");
+
+		$(thisCurrentCountBlock).text('1');
+		$(thisAllCountBlock).text(thisElemSlidesCount);
+
+		$(thisElem).slick({
+			slidesToShow: 1,
+			infinite: true,
+			arrows: true,
+			prevArrow: '<a href="javascript:;" class="footer__portfolio-back prev"><img src="img/sliderArrowRightBlue.svg"></img></a>',
+			nextArrow: '<a href="javascript:;" class="footer__portfolio-forward next"><img src="img/sliderArrowRightBlue.svg"></img></a>',
+			dots: false,
+			responsive: [
+				{
+					breakpoint: 768,
+					settings: {
+						arrows: false,
+						dots: true,
+						adaptiveHeight: true
+					}
+				}
+			]
+		});
+
+		$(thisElem).on('afterChange', function(e, slick, currentSlide) {
+			$(thisCurrentCountBlock).text(currentSlide + 1);
+		});
+	});
+}
